12cf5d0ecd0df5ff7e483b912f6dec031c8a8f82
brettkolodny <brettkolodny@gmail.com>
2022-08-29 12:31:31 -0400

    fix: config for custom domain

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,7 +33,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "todoi.sh"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :todoish, TodoishWeb.Endpoint,
